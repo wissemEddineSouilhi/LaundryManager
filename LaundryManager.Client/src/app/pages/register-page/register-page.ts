69c47fd6129962fc598f2d04ad4d4242aa2a99de
wissemEddineSouilhi <wissemeddinesouilhi@hotmail.com>
2025-07-10 00:34:40 +0200

    add registration

---
+++ LaundryManager.Client/src/app/pages/register-page/register-page.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import {Client, CreateUserDto, LoginDto} from '../../api/api-client'
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators,FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabel } from 'primeng/floatlabel';
+
+@Component({
+  selector: 'app-register-page',
+  imports: [FormsModule,ReactiveFormsModule,CardModule,InputTextModule,PasswordModule,ButtonModule],
+  templateUrl: './register-page.html',
+  styleUrl: './register-page.css'
+})
+export class RegisterPage {
+
+
+
+   registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder,  private client: Client, private router: Router,) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      phoneNumber: ['', Validators.required] // matches updated DTO
+    });
+  }
+
+   onSubmit() {
+    if (this.registerForm.valid) {
+      const formValues = this.registerForm.value;
+      const newUser = new CreateUserDto(formValues);
+
+      this.client.register(newUser).subscribe({
+       next: (response) => {
+        alert('User registered successfully!')
+        
+      },
+      error: (err) => {
+        
+        alert('Registration failed: ' + err.message)
+      },
+    });      
+    }
+  }
+
+}
